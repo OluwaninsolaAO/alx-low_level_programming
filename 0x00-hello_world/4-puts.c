f551889687394bd18f52f312c5d863fd16d38160
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-08 21:56:24 +0000

    Task 04: Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * This is my first c written in vi
+ * Maybe my second
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
