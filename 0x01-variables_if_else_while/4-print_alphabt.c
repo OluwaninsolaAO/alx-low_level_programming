20f964caf0130293ddc3b9f9f6cedd9af5425804
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-09 22:31:38 +0000

    Task 04

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the programme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
