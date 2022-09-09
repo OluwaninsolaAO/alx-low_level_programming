ade268872889f1fab1345dac40445d1d003bc960
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-09 23:00:03 +0000

    Task 07: Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the programme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'z'; letter--)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
