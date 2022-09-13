211f18a33a8c18eae03cfefb6a97235060ae9449
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-13 23:11:05 +0000

    Task 06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Return: Abosolute value
+ *
+ * @n: automatic variable
+ *
+ */
+
+int _abs(int n)
+{
+	n *= -1;
+
+	return (n);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int r;
-	r = print_sign(0);
+	r = _abs(-9);
 	_putchar(r + '0');
 	_putchar('\n');
 
