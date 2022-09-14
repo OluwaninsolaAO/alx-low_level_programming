088a757c0529193d74cbc63240527e78c0b1644a
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-14 01:17:22 +0000

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,13 +13,19 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
+		printf("98");
 	}
 	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 			printf("%d, ", i);
+		printf("98");
+	}
+	else if (n = 98)
+	{
+		printf("98");
 	}
 
 	_putchar('\n');
