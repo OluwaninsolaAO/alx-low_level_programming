b99fcb5ad5f89cf95fe5f28ca39d1d4169dd7337
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-15 23:05:56 +0000

    Task 02

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * main - function to test another function
+ * Return: just 0
+ */
+int main(void)
+{
+	printf("%d is the largest number\n", largest_number(972, -98, 0));
+
+	return (0);
+}
