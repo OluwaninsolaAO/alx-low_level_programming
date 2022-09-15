7f09df59a9786327535475d788dc156fa2615380
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-15 20:31:44 +0000

    I am lazy, honestly

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_most_numbers - function that prints the numbers, from 0 to 9,
  * followed by a new line.
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	int c;
