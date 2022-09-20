64f1613a23fa9104f1b22f900cea8fbf2bf5bb9a
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-20 00:11:52 +0000

    Task 07

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half portion of string of any lenght
+ * Return: Null
+ * @str: victim
+ */
+
+void puts_half(char *str)
+{
+	int count, i;
+
+	count = 0;
+
+	while (str[count] != '\0')
+		count++;
+
+	if (count % 2 != 0)
+		count -= 1;
+
+	for (i = 0; i <= (count / 2); i++)
+		_putchar(str[i + (count / 2)]);
+	_putchar('\n');
+}
