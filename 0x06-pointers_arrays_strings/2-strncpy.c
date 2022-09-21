f37bacf5d11126027defdd3a6174ef5abb534ba6
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-21 23:34:09 +0000

    Task 02

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int count_string(char *str);
+
+/**
+ * _strncpy - a copy of strncpy.
+ * @dest: pointer to the destination string.
+ * @src: pointer to the source string.
+ * @n: limit stop.
+ * Return: updated pointer to the destination string [dest].
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dest_lenght, i;
+
+	dest_lenght = count_string(dest);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	if (n > dest_lenght)
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
+
+/**
+ * count_string - Count  number of char in a string excluding '\0'
+ * Return: total string lenght
+ * @str: being the string to count
+ */
+int count_string(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	return (count);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,13 +7,12 @@
 
 int main(void)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "Wolrd!\n";
+	char s1[98] = "Hello there, my name is Oluwaninsola.";
 	char *ptr;
 
-	ptr = _strncat(s1, s2, 3);
+	ptr = _strncpy(s1, "123456789", 8);
 
-	printf("%s", ptr);
+	printf("%s\n", ptr);
 
 	return (0);
 }
