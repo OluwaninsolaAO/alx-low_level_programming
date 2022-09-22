841d0084eed01dc33626a41689ec1a843ee1f809
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-22 19:37:36 +0000

    Task 08

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,27 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
+ * @str: An input string to encode using rot13
  * Return: An encode string
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		while ((str[i] >= 'a' && str[i] <= 'z') ||
+				(str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
+			if ((str[i] >= 'a' && str[i] <= 'm') ||
+					(str[i] >= 'A' && str[i] <= 'M'))
+				str[i] += 13;
 			else
-				s[i] -= 13;
+				str[i] -= 13;
 			i++;
 		}
 		i++;
 	}
-	return (s);
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *rot13(char *str);
+void print_number(int n);
 
 #endif
