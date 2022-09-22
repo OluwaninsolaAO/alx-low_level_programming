ff247c22c88a46f0c60ad5a164c7ef0aa996e851
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-22 00:25:17 +0000

    Task 02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,13 @@ int count_string(char *str);
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, src_lenght = count_string(src);
-
-	if (n > src_lenght)
-		n = src_lenght;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = src[i];
+
+	for ( ; i <= n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
