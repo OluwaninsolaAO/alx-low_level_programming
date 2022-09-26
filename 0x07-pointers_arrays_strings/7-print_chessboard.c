e9f5a360ab21a3eeebbd293b5e4a153fe078a8d2
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-26 22:35:16 +0000

    Task 06 Actually

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#incldue "main.h"
+#include "main.h"
 
 /**
  * print_chessboard - print chess
@@ -9,7 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
