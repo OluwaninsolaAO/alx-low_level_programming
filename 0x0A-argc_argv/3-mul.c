0140998dd32d597f91cdfea505683f3de870b3e0
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-09-30 23:59:39 +0000

    Task 03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies 2 numbers
@@ -10,13 +11,18 @@
 
 int main(int argc, char *argv[])
 {
+	int a, b;
+
 	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", (*argv[1] * *argv[2]));
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
 
 	return (0);
 }
