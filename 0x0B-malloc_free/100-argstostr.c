4d1c1ad5ca18cd337eab83f79d348b18cf67e794
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-04 19:30:25 +0000

    Task 05

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * argstostr - Concatenates args to  array
+ * @ac: count
+ * @av: vector
+ * Return: ptr to array
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i = 0, j, k, count = 0;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+
+	while (i < ac)
+	{
+		j = 0;
+		while (av[j] != '\0')
+		{
+			count++;
+			j++;
+		}
+		i++;
+	}
+
+	s = malloc((count + ac + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	k = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[j] != '\0')
+		{
+			s[k] = av[j]
+
