61e7f52954ed6260c4fed644545e995b13d0b6a4
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-04 19:07:48 +0000

    Task 04

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,9 +10,11 @@
 void free_grid(int **grid, int height)
 {
 	int i = 0;
+
+	if (grid == NULL)
+		return;
+
 	while (i < height)
-	{
-		free(grid[i]);
-		i++;
-	}
+		free((int *) grid[i++]);
+	free(grid);
 }
