f6d6a3b7f44d33a3eed2d67cae05b7806e189aee
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-06 21:48:10 +0000

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,18 +11,22 @@ int main(int argc, char **argv)
 {
 	int num1, num2, result;
 
-	if (argc != 3)
+	if (argc < 3 || argc >3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 || num2 == 0)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
+
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
 
