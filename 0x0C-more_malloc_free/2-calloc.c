b90eb1f78bfb3f4dbdc2a32598fc059421957383
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-06 20:09:25 +0000

    Task 02

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocate and intialize with 0x00
+ * array of size nmemb of size bytes
+ * @nmemb: size of the array.
+ * @size: byte size of each elements
+ * Return: Pointer to the array.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
+
