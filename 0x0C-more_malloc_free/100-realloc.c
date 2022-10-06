2a145fdc567fb7f737f39a4ebe4bbf0be4fb7e61
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-10-06 22:31:51 +0100

    Advance

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,10 +7,31 @@
  * @new_size: new string size.
  * Return: pointer to the new allocated memory.
  */
-
-void *_realloc(void *ptr __attribute__ ((unused)), unsigned int old_size __attribute__ ((unused)), unsigned int new_size __attribute__ ((unused)))
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	printf("%p\n", ptr);
-	printf("%p\n", ptr + 1);
-	return (ptr);
+	char *new_location, *new_ptr;
+	unsigned int i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL) /* treat as normal malloc */
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_location = malloc(new_size);
+	new_ptr = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		new_location[i] = new_ptr[i];
+	free(ptr);
+	return (new_location);
 }
