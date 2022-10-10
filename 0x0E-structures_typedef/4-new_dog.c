6285ac6a4b8a7302cb706e764a3b6f8ae396df76
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-10-11 00:18:54 +0100

    Task 04

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,22 +9,38 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr, doggy;
-	char *cpy_name, *cpy_owner;
+	dog_t *new_dog;
+	char *cpyname, *cpyowner;
+	int len_name = 0, len_owner = 0, i;
 
-	ptr = &doggy;
-
-	if (name == NULL || *name == '\0')
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	if (age < 0 || owner == NULL || *owner == '\0')
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	cpy_name = name;
-	cpy_owner = owner;
 
-	ptr->name = cpy_name;
-	ptr->age = age;
-	ptr->owner = cpy_owner;
+	cpyname = malloc(len_name + 1);
+	if (cpyname == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+	cpyname[i] = '\0';
 
-	return (ptr);
+	cpyowner = malloc(len_owner + 1);
+	if (cpyowner == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+	cpyowner[i] = '\0';
+
+	new_dog->name = cpyname;
+	new_dog->age = age;
+	new_dog->owner = cpyowner;
+	return (new_dog);
 }
