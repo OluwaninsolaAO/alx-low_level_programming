825c932359bae0f5c055b79026fdc24924504f7e
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-10 23:10:42 +0000

    Task 04

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,18 +10,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr, doggy;
+	char *cpy_name, *cpy_owner;
 
 	ptr = &doggy;
 
 	if (name == NULL || *name == '\0')
-		exit(404);
+		return (NULL);
 
 	if (age < 0 || owner == NULL || *owner == '\0')
-		exit(404);
+		return (NULL);
+	cpy_name = name;
+	cpy_owner = owner;
 
-	ptr->name = name;
+	ptr->name = cpy_name;
 	ptr->age = age;
-	ptr->owner = owner;
+	ptr->owner = cpy_owner;
 
 	return (ptr);
 }
