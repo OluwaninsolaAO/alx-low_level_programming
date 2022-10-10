b1c9cbd56840ad9f7f269ce7e22a94c12e93a64d
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-10 21:28:18 +0000

    Task 00

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - defines a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: onwer of dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
