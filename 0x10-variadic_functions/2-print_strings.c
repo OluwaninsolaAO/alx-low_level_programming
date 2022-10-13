b679e155b29271d6ef598bf11a652b83cdf9c207
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-13 22:22:16 +0000

    Task 02

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,11 +7,12 @@
  * @n: number of passed numbers.
  * Return: Nothing
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
 	const char *c;
+	char *holder;
 
 	if (separator == NULL)
 		c = "";
@@ -21,8 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(ap, char) != NULL)
-			printf("%s", va_arg(ap, char));
+		holder = va_arg(ap, char*);
+		if (holder != NULL)
+			printf("%s", holder);
 		else
 			printf("(nil)");
 
