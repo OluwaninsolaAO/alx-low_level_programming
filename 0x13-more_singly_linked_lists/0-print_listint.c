77a2966a11920778a9f9274d7e7969c915da65c8
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-25 21:46:38 +0000

    Task 00

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,10 @@ size_t print_listint(const listint_t *h)
 {
 	int num = 1;
 
-	if (h != NULL)
-	{
-		printf("%d\n", h->n);
-		return (num + print_listint(h->next));
-	}
-	return (0);
+	if (h == NULL)
+		return (0);
+	
+	printf("%d\n", h->n);
+	
+	return (num + print_listint(h->next));
 }
