41fe28c14e5891d1cc50ee0d55de03a8e7e66e9d
OluwaninsolaAO <olagunjusola070@gmail.com>
2022-10-25 23:59:58 +0000

    Task 05

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory of linked list part 2
+ * @head: linked list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head, *ptr;
+
+	while (temp)
+	{
+		ptr = temp;
+		temp = temp->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
