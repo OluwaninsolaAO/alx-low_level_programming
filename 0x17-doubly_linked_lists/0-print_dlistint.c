4630420d448220fdafe7c1f444c2a1263158057a
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-08 19:58:52 +0000

    Task 00

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,12 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t num = 0;
 	const dlistint_t *copy = h;
 
-	while (copy->next != NULL)
+	while (copy)
 	{
 		printf("%d\n", copy->n);
 		num++;
 		copy = copy->next;
 	}
-	printf("%d\n", copy->n);
-	return (num + 1);
+	return (num);
 }
