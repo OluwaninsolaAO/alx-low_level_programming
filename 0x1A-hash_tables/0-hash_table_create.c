1d329055cf5f9e56e0c1ef0366c62cc73e52c136
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-06 19:31:34 +0000

    Task 00

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	/**
+	 * NOTE TO SELF:
+	 * why malloc for ``sizeof(hash_node_t *)``?
+	 * ANS: ``array`` is an array of pointers, the initial
+	 * ``sizeof(hash_node_t)`` allocates space for the
+	 * typedef of hash_node_t (8 * 3 = 24 bytes) whereas
+	 * ``sizeof(hash_node_t *)`` allocates space for
+	 * pointer to hash_node_t (8 bytes).
+	 */
 	if (ht->array == NULL)
 	{
 		free(ht);
